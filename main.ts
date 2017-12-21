b0a83d8b37b3a9ffbc8d449c70b20c9b3f4c793e
Neko <469867876@qq.com>
2017-12-21 10:52:25 +0800

    [FEATURE] 静态文件服务缓存 & gzip

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
 const path = require("path")
+const compress = require('koa-compress')
 const Koa = require('koa')
-const static = require('koa-static')
+const staticCache = require('koa-static-cache')
 const bodyParser = require('koa-bodyparser')
 const router = require('./router/index.ts')
 const app = new Koa()
-const staticPath = './static'
+const staticPath = path.join(__dirname, './public')
 
 app
     .use(bodyParser())
+    // gzip
+    .use(compress({
+        filter: function (content_type) {
+            return /text/i.test(content_type)
+        },
+        threshold: 1024,
+        flush: require('zlib').Z_SYNC_FLUSH
+      }))
     .use(router.routes())
     .use(router.allowedMethods())
-    .use(static(path.join(__dirname, staticPath)))
-
+    // 静态文件
+    .use(staticCache(staticPath, { maxAge: 365 * 24 * 60 * 60 }))
+    
 app.listen(80)
--- router/index.ts
+++ router/index.ts
@@ -2,9 +2,8 @@ const controler = require('../controler/index.ts')
 const Router = require('koa-router')
 const route = new Router()
 
-route.get('/', async (ctx, next) => {
+route.get('/test', async (ctx, next) => {
     ctx.body = await controler.index()
-
 })
 
 module.exports = route
